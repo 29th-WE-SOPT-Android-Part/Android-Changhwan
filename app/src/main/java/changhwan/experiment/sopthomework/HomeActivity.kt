8e6a9f1fe128a97815fcfd42004e903d7b9cbf1c
leeChangHwan <chang5hustle@gmail.com>
2021-10-10 22:11:19 +0900

    level3 no image

--- app/src/main/java/changhwan/experiment/sopthomework/HomeActivity.kt
+++ app/src/main/java/changhwan/experiment/sopthomework/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import changhwan.experiment.sopthomework.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -12,12 +13,21 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        val introduce = Introduce(
+            "이창환",
+            26,
+            "외워야겠다...",
+            "현재시각 새벽 2시에 푸라닭을 결국 먹고있는 저는 돼지입니다 제발 살은 안쪘으면 좋겠네요"
+        )
+
+        binding.introduce = introduce
 
         binding.homeToGit.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/2chang5"))
             startActivity(intent)
         }
+
     }
 }
---
+++ app/src/main/java/changhwan/experiment/sopthomework/Introduce.kt
@@ -0,0 +1,8 @@
+package changhwan.experiment.sopthomework
+
+data class Introduce(
+    val name: String,
+    var age: Int,
+    val mbti: String,
+    val introduction: String,
+)
