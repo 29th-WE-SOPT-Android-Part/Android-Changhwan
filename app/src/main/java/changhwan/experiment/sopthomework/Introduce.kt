2179c7eb576d7d3844f6190c731780872233a4db
leeChangHwan <chang5hustle@gmail.com>
2021-10-19 10:38:31 +0900

    livedatadone

--- app/src/main/java/changhwan/experiment/sopthomework/Introduce.kt
+++ app/src/main/java/changhwan/experiment/sopthomework/Introduce.kt
@@ -1,11 +1,12 @@
 package changhwan.experiment.sopthomework
 
 import android.graphics.drawable.Drawable
+import androidx.lifecycle.MutableLiveData
 
 data class Introduce(
-    val name: String,
-    var age: Int,
-    val mbti: String,
-    val introduction: String,
-    val resorce : Int,
+    var liveName: MutableLiveData<String>,
+    var LiveAge: MutableLiveData<Int>,
+    val liveMbti: MutableLiveData<String>,
+    val liveIntroduction: MutableLiveData<String>,
+    val resorce: Int,
 )
