cf9dc6e83f0a7206fdd2413d83a1a9b802e1a695
changhwan <chang5hustle@gmail.com>
2021-11-10 21:29:00 +0900

    week4 2-1 help me

--- app/src/main/java/changhwan/experiment/sopthomework/ServiceCreator.kt
+++ app/src/main/java/changhwan/experiment/sopthomework/ServiceCreator.kt
@@ -4,13 +4,24 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceCreator {
-    private const val  BASE_URL= "https://asia-northeast3-we-sopt-29.cloudfunctions.net/api/"
+    private const val  SOPT_BASE_URL= "https://asia-northeast3-we-sopt-29.cloudfunctions.net/api/"
 
-    private val retrofit :Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
+    private val SoptRetrofit :Retrofit = Retrofit.Builder()
+        .baseUrl(SOPT_BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    val signUpService :SignUpService = retrofit.create(SignUpService::class.java)
-    val signInService :SignInService = retrofit.create(SignInService::class.java)
+    val signUpService :SignUpService = SoptRetrofit.create(SignUpService::class.java)
+    val signInService :SignInService = SoptRetrofit.create(SignInService::class.java)
+
+
+    private const val  GITHUB_BASE_URL="https://api.github.com/"
+
+    private  val GitHubRetrofit : Retrofit = Retrofit.Builder()
+        .baseUrl(GITHUB_BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+
+    val gitHubService :GitHubService = GitHubRetrofit.create(GitHubService::class.java)
 }
